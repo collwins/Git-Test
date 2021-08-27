b87b636e617585f839ab413b9792e06e7f2a787e
collwins <collwins@ut.utm.edu>
2021-08-26 20:22:42 -0500

    Guessing game ++

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -11,10 +11,14 @@ namespace GitProject
 		static void Main(string[] args)
 		{
 			var rand = new Random();
-			int number = rand.Next(101);
-			int guess = 200;
 			int tries = 0;
-			Console.WriteLine("I'm thinking of a number between 0 and 100. Try to guess it!");
+			Console.WriteLine("Enter the lower bound in your guessing range: ");
+			int lower = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter the higher bound in your guessing range: ");
+			int higher = Convert.ToInt32(Console.ReadLine());
+			int number = rand.Next(lower, higher + 1);
+			int guess = higher + 20;
+			Console.WriteLine($"I'm thinking of a number between {lower} and {higher}. Try guessing it!");
 			while (guess != number)
 			{
 				tries++;
