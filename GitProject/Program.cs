e1b2eddb141afcffe43731e45881e87dfdb500cf
collwins <collwins@ut.utm.edu>
2021-08-26 20:02:14 -0500

    Guessing game.

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace GitProject
 {
@@ -10,7 +10,28 @@ namespace GitProject
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World");
+			var rand = new Random();
+			int number = rand.Next(101);
+			int guess = 200;
+			int tries = 0;
+			Console.WriteLine("I'm thinking of a number between 0 and 100. Try to guess it!");
+			while (guess != number)
+			{
+				tries++;
+				guess = Convert.ToInt32(Console.ReadLine());
+				if (guess > number)
+				{
+					Console.WriteLine("Too high!");
+				}
+				else if (guess < number)
+				{
+					Console.WriteLine("Too low!");
+				}
+			}
+			Console.WriteLine("That's right!");
+			Console.Write("Total guesses: ");
+			Console.Write(tries);
+			Thread.Sleep(2000);
 		}
 	}
 }
